375c69da70404aa5f5ce2e0d1214eb6ddc9abdb6
Jeremy Parris <jeremy.parris@a-f-p.com>
2018-11-14 15:49:56 -0600

    Installed entity framework

---
+++ SalesForce/SalesForce/Data/SalesforceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SalesForce.Data
+{
+    public class SalesforceContext : DbContext
+    {
+        public SalesforceContext() : base("Salesforce")
+        {
+
+        }
+    }
+}
---
+++ SalesForce/SalesForce/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SalesForce.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SalesForce.Data.SalesforceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SalesForce.Data.SalesforceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
