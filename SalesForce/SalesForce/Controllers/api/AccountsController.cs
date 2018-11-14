ff9ed78b11f04ae95222f02d96abdeb8ba299d2c
Jeremy Parris <jeremy.parris@a-f-p.com>
2018-11-14 17:04:57 -0600

    accounts api controller

---
+++ SalesForce/SalesForce/Controllers/api/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using SalesForce.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SalesForce.Controllers.api
+{
+    public class AccountsController : ApiController
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public AccountsController()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public IHttpActionResult Get()
+        {
+            var accounts = _unitOfWork._accountRepository.Get();
+
+            return Json(accounts);
+        }
+    }
+}
