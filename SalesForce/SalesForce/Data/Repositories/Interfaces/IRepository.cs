c23c29794dd7afce35146ee5e887062997d7bbec
Jeremy Parris <jeremy.parris@a-f-p.com>
2018-11-14 16:13:06 -0600

    generic repository

---
+++ SalesForce/SalesForce/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesForce.Data.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        T GetById(int id);
+    }
+}
---
+++ SalesForce/SalesForce/Data/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+﻿using SalesForce.Data.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace SalesForce.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _context.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+    }
+}
